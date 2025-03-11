0cbd26ab20e2528db2b5f89d6c415e34203b64f0
YMH <asds2534@naver.com>
2025-03-11 11:14:39 +0900

    3색 LED로 무드등 시스템 구현

--- LED/3Color_LED/3Color_LED.ino
+++ LED/3Color_LED/3Color_LED.ino
@@ -1,14 +1,19 @@
 #define RED_PIN 11
 #define GREEN_PIN 10
 #define BLUE_PIN 9
+#define TOUCH_STATE 7
+void colorPrint(int redValue, int greenValue, int blueValue);
 
 void setup() {
   pinMode(RED_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
   pinMode(BLUE_PIN, OUTPUT);
+  pinMode(TOUCH_STATE, INPUT);
 }
 
 void loop() {
+
+  int touchValue = digitalRead(TOUCH_STATE);
   // 빨간색 출력
   analogWrite(RED_PIN, 255);
   analogWrite(GREEN_PIN, 0);
